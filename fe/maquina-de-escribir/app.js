eb1abedd640ea036dfd030ef1eb8ec1535c00a60
DavidGMont <davidgarmo@outlook.com>
2023-01-10 22:03:59 -0500

    Cargando el reto #3

---
+++ fe/maquina-de-escribir/app.js
@@ -0,0 +1,47 @@
+function dormir(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const frases = [
+	"hablar con Serra",
+	"escribir código",
+	"chismosear",
+	"escuchar música",
+	"resolver retos",
+	"aprender",
+	"bailar",
+	"transformarme",
+];
+const elemento = document.getElementById("maquina");
+
+let tiempoDormida = 100;
+
+let indiceFraseActual = 0;
+
+const escribirCiclo = async () => {
+	while (true) {
+		let palabraActual = frases[indiceFraseActual];
+
+		for (let i = 0; i < palabraActual.length; i++) {
+			elemento.innerText = palabraActual.substring(0, i + 1);
+			await dormir(tiempoDormida);
+		}
+
+		await dormir(tiempoDormida * 10);
+
+		for (let i = palabraActual.length; i > 0; i--) {
+			elemento.innerText = palabraActual.substring(0, i - 1);
+			await dormir(tiempoDormida);
+		}
+
+		await dormir(tiempoDormida * 5);
+
+		if (indiceFraseActual === frases.length - 1) {
+			indiceFraseActual = 0;
+		} else {
+			indiceFraseActual++;
+		}
+	}
+};
+
+escribirCiclo();
